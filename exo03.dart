a303501938fec1f25232b4c55e34ab41508c63cc
LAGGAB KHALEF <laggabkhalef32@gmail.com>
2025-11-14 19:05:14 -0500

    mplémentez un Getter public solde qui retourne la valeur de _solde formatée en dollar

--- exo03.dart
+++ exo03.dart
@@ -1,5 +1,7 @@
 class Compte {
   final String numeroCompte;
   double _solde;
-  Compte(this.numeroCompte, [this._solde = 0.0]);
-  }
+  Compte(tgit .
+  his.numeroCompte, [this._solde = 0.0]);
+  String get solde => '${_solde.toStringAsFixed(2)} \$';
+}
