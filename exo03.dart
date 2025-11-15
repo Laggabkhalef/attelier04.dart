1fea2746a015e01e389db09360ebf38f1ce2730c
LAGGAB KHALEF <laggabkhalef32@gmail.com>
2025-11-14 19:11:03 -0500

    Dans main(), créez un compte

--- exo03.dart
+++ exo03.dart
@@ -1,11 +1,15 @@
 class Compte {
   final String numeroCompte;
   double _solde;
-  Compte(tgit .
-  his.numeroCompte, [this._solde = 0.0]);
+  Compte(this.numeroCompte, [this._solde = 0.0]);
   String get solde => '${_solde.toStringAsFixed(2)} \$';void depot(double montant) {
     if (montant > 0) {
       _solde += montant;
     }
   }
+}
+void main() {
+  var compte = Compte('12345');
+  compte.depot(150);
+  print('Solde du compte: ${compte.solde}');
 }
