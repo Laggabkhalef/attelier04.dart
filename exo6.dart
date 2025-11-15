c47ca92de9c7647e9e79e0d60e1a9cdd6a0650f7
LAGGAB KHALEF <laggabkhalef32@gmail.com>
2025-11-14 20:30:19 -0500

    Créez une classe BaseDeDonnees qui implémente également Connectable et affiche ses propres messages dans les deux méthodes

--- exo6.dart
+++ exo6.dart
@@ -2,3 +2,15 @@ abstract class Connectable {
   void connecter(String utilisateur);
   void deconnecter();
 }
+class ServeurAPI implements Connectable {
+  @override
+  void connecter(String utilisateur) {
+    print('ServeurAPI : Connexion établie pour $utilisateur.');
+  }
+  }
+  class BaseDeDonnees implements Connectable {
+  @override
+  void connecter(String utilisateur) {
+    print('BaseDeDonnees : Utilisateur $utilisateur connecté.');
+  }
+  }
