f9636149ae05d68fad0e910a20ce6551dcce8f23
LAGGAB KHALEF <laggabkhalef32@gmail.com>
2025-11-14 20:18:29 -0500

    Appelez afficherMessage et calculerAire

--- exo5.dart
+++ exo5.dart
@@ -11,4 +11,21 @@ class Cercle extends Forme {
 
   @override
   double calculerAire() => 3.14159 * rayon * rayon;
+}class Rectangle extends Forme {
+  final double longueur, largeur;
+  Rectangle(this.longueur, this.largeur);
+
+  @override
+  double calculerAire() => longueur * largeur;
+}
+
+void main() {
+  var cercle = Cercle(3);
+  var rectangle = Rectangle(4, 5);
+
+  cercle.afficherMessage();
+  print('Aire cercle: ${cercle.calculerAire()}');
+
+  rectangle.afficherMessage();
+  print('Aire rectangle: ${rectangle.calculerAire()}');
 }
